b50e223cfde8f9d333ff505ef4bbd100ba13e898
jatobro <jakob.tossebro@gmail.com>
2023-11-06 21:06:22 +0100

    functionality required in place

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [topic, setTopic] = useState<string>("");
+  const [mentions, setMentions] = useState<number | null>(null);
+
+  const getTopicMentionCount = async (topic: string) => {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=parse&section=0&prop=text&format=json&page=${topic}`)
+      .then(response => response.json())
+      .then(data => {
+        const text = data.parse.text['*'];
+        const regex = new RegExp(topic, 'gi');
+
+        const mentions = (text.match(regex) || []).length;
+        console.log(`Topic was mentioned ${mentions} times`);
+
+        return mentions;
+      })
+      .catch((error) => console.log(error))
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const mentions = await getTopicMentionCount(topic);
+    setMentions(mentions);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <label>
+          What topic do you want to get the amount for?
+          <input type="text" name="topic" value={topic} onChange={(e) => setTopic(e.target.value)} />
+        </label>
+        <button type="submit">Get amount</button>
+
+      </form>
+      <p>{mentions ?? ""}</p>
     </div>
+
   );
 }
 
